f3a87cccc1dc34086864dd074cc497a2a7499040
TT375S <tomataka375@gmail.com>
2017-01-28 11:35:42 +0900

    Arrange the appearance. 体裁を整えました。完全に動くようになりました。

--- files/search.py
+++ files/search.py
@@ -7,14 +7,15 @@ timetable  = "timetable.txt"
 A_schedule = "d_FA.txt"
 C_schedules = ["d_FC_kikan.txt", "d_FC_sensin.txt", "d_FC_souzou.txt"]
 
+#受講している授業の一覧を引数で指定
 if len(args) > 1 :
 	timetable = args[1]
 	for i in range(3):
 		C_schedules[i] = args[i+2]
 
-
 f_timeTable = open(timetable)
 
+#使うファイル、時間割と各テストのスケジュールファイルを開く
 f_schedules = []
 def openfiles():
 	global f_schedules
@@ -25,7 +26,7 @@ def openfiles():
 	
 openfiles()
 		
-
+#授業名から、スケジュールを検索する
 foundSchedules = []
 def searchSchedule(className):
 	global foundSchedules
@@ -37,18 +38,26 @@ def searchSchedule(className):
 				return True
 	return False
 
+#授業名一覧から全て検索していく
+print "-------授業名一覧です。-------"
+print "*...テストのある授業\n-...テストのない授業\n"
 className = f_timeTable.readline()
 while className:
 	openfiles()
-#	print className.strip()
 	if not searchSchedule(className):
-		print className.strip()	
+		print "- "+ className.strip()
+	else:
+		print "* "+ className.strip()
 	className = f_timeTable.readline()
 
-print "Content-type: text/html; charset=utf-8\n"
-print """<html><meta charset="utf-8"><body>"""
+print "\n-------テストスケジュール一覧--------\n"
+
+#print "Content-type: text/html; charset=utf-8\n"
+#print """<html><meta charset="utf-8"><body>"""
 
 for schedule in foundSchedules:
+	if schedule.find("Schedule") != -1:
+		schedule = schedule[schedule.find("Schedule")+len("Schedule")+1:]
 	print schedule
 
-print "</body></html>"""
+#print "</body></html>"""
